0dda7d4e765e2bf55344635c42dc6960038759e4
e1093051 <e1093051@u.nus.edu>
2023-06-03 16:50:57 +0800

    Changes on Personal Profile Page

--- src/routes/Form.js
+++ src/routes/Form.js
@@ -4,6 +4,9 @@ import { useNavigation, useRoute } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { ActivityIndicator } from 'react-native';
 import { Dropdown } from 'react-native-element-dropdown';
+import { MultiSelect } from 'react-native-element-dropdown';
+import AntDesign from '@expo/vector-icons/AntDesign';
+
 
 import * as ImagePicker from 'expo-image-picker';
 
@@ -27,8 +30,16 @@ export default () => {
 
   const [gender, setGender] = useState("");
   const [major, setMajor] = useState("");
-  const [module, setModule] = useState("");
+  const [module, setModule] = useState([]);
   const [moduleData, setModuleData] = useState([]);
+
+  const renderItem = item => {
+    return (
+      <View style={styles.item}>
+        <Text style={styles.selectedTextStyle}>{item.label}</Text>
+      </View>
+    );
+  };
 
 
 
@@ -142,7 +153,7 @@ export default () => {
           maxHeight={300}
           labelField="label"
           data={genderData}
-          onChange={item => setGender(item.label)}
+          onChange={item => setGender(item.value)}
           value={gender}
           placeholder=" "
           valueField="value"
@@ -153,6 +164,7 @@ export default () => {
         />
         <Text style={styles.usual}>Major</Text>
         <Dropdown
+          search
           style={styles.dropdown}
           placeholderStyle={styles.placeholderStyle}
           selectedTextStyle={styles.selectedTextStyle}
@@ -160,25 +172,39 @@ export default () => {
           maxHeight={300}
           labelField="label"
           data={majorData}
-          onChange={item => setMajor(item.label)}
+          onChange={item => setMajor(item.value)}
           value={major}
           placeholder=" "
           valueField="value"
         />
         <Text style={styles.usual}>Course</Text>
-        <Dropdown
-          style={styles.dropdown}
-          placeholderStyle={styles.placeholderStyle}
-          selectedTextStyle={styles.selectedTextStyle}
-          inputSearchStyle={styles.inputSearchStyle}
-          maxHeight={300}
-          labelField="label"
-          data={moduleData}
-          onChange={item => setModule(item.value)}
-          value={module}
-          placeholder=" "
-          valueField="value"
-        />
+        <MultiSelect
+        style={styles.dropdown}
+        placeholderStyle={styles.placeholderStyle}
+        selectedTextStyle={styles.selectedTextStyle}
+        inputSearchStyle={styles.inputSearchStyle}
+        iconStyle={styles.iconStyle}
+        data={moduleData}
+        labelField="label"
+        valueField="value"
+        placeholder=" "
+        value={module}
+        search
+        searchPlaceholder="Search..."
+        onChange={item => {
+          setModule(item);
+        }}
+        selectedStyle = {{margin: 16}}
+        renderItem={renderItem}
+        renderSelectedItem={(item, unSelect) => (
+          <TouchableOpacity onPress={() => unSelect && unSelect(item)}>
+            <View style={styles.selectedStyle}>
+              <Text style={styles.textSelectedStyle}>{item.label}</Text>
+              <AntDesign color="black" name="delete" size={16} />
+            </View>
+          </TouchableOpacity>
+        )}
+      />
       </View>
     </View>
   );
@@ -214,9 +240,7 @@ const styles = StyleSheet.create({
     borderBottomWidth: 0.5,
     width: Dimensions.get('window').width - 32,
   },
-  icon: {
-    marginRight: 5,
-  },
+
   placeholderStyle: {
     fontSize: 16,
   },
@@ -235,5 +259,178 @@ const styles = StyleSheet.create({
     borderBottomWidth: 0.5,
     width: Dimensions.get('window').width - 32,
     fontSize: 16,
-  }
+  },
+  icon: {
+    marginRight: 5,
+  },
+  item: {
+    padding: 17,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  selectedStyle: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 14,
+    backgroundColor: 'white',
+    shadowColor: '#000',
+    marginTop: 8,
+    marginRight: 12,
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+
+    elevation: 2,
+  },
+  textSelectedStyle: {
+    marginRight: 5,
+    fontSize: 16,
+  },
 });
+
+
+/** 
+import React, { useState } from 'react';
+import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
+import { MultiSelect } from 'react-native-element-dropdown';
+import AntDesign from '@expo/vector-icons/AntDesign';
+
+const data = [
+  { label: 'Item 1', value: '1' },
+  { label: 'Item 2', value: '2' },
+  { label: 'Item 3', value: '3' },
+  { label: 'Item 4', value: '4' },
+  { label: 'Item 5', value: '5' },
+  { label: 'Item 6', value: '6' },
+  { label: 'Item 7', value: '7' },
+  { label: 'Item 8', value: '8' },
+];
+
+const MultiSelectComponent = () => {
+  const [selected, setSelected] = useState([]);
+
+  const renderItem = item => {
+    return (
+      <View style={styles.item}>
+        <Text style={styles.selectedTextStyle}>{item.label}</Text>
+        <AntDesign style={styles.icon} color="black" name="Safety" size={20} />
+      </View>
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <MultiSelect
+        style={styles.dropdown}
+        placeholderStyle={styles.placeholderStyle}
+        selectedTextStyle={styles.selectedTextStyle}
+        inputSearchStyle={styles.inputSearchStyle}
+        iconStyle={styles.iconStyle}
+        data={data}
+        labelField="label"
+        valueField="value"
+        placeholder="Select item"
+        value={selected}
+        search
+        searchPlaceholder="Search..."
+
+        onChange={item => {
+          setSelected(item);
+        }}
+        renderLeftIcon={() => (
+          <AntDesign
+            style={styles.icon}
+            color="black"
+            name="Safety"
+            size={20}
+          />
+        )}
+        renderItem={renderItem}
+        renderSelectedItem={(item, unSelect) => (
+          <TouchableOpacity onPress={() => unSelect && unSelect(item)}>
+            <View style={styles.selectedStyle}>
+              <Text style={styles.textSelectedStyle}>{item.label}</Text>
+              <AntDesign color="black" name="delete" size={17} />
+            </View>
+          </TouchableOpacity>
+        )}
+      />
+    </View>
+  );
+};
+
+export default MultiSelectComponent;
+
+const styles = StyleSheet.create({
+  container: { padding: 16 },
+  dropdown: {
+    height: 50,
+    backgroundColor: 'white',
+    borderRadius: 12,
+    padding: 12,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+
+    elevation: 2,
+  },
+  placeholderStyle: {
+    fontSize: 16,
+  },
+  selectedTextStyle: {
+    fontSize: 14,
+  },
+  iconStyle: {
+    width: 20,
+    height: 20,
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 16,
+  },
+  icon: {
+    marginRight: 5,
+  },
+  item: {
+    padding: 17,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  selectedStyle: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 14,
+    backgroundColor: 'white',
+    shadowColor: '#000',
+    marginTop: 8,
+    marginRight: 12,
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+
+    elevation: 2,
+  },
+  textSelectedStyle: {
+    marginRight: 5,
+    fontSize: 16,
+  },
+});
+*/
