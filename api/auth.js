89503c281f409721fcee6648e0415d5ecbd3c5f6
e1093051 <e1093051@u.nus.edu>
2023-06-06 17:24:32 +0800

    Password Reset

--- api/auth.js
+++ api/auth.js
@@ -3,7 +3,7 @@
 import { auth, firebase } from './fireConfig'
 import { Alert } from 'react-native';
 
-import { signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile, sendEmailVerification, signOut } from 'firebase/auth';
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile, sendEmailVerification, signOut, sendPasswordResetEmail } from 'firebase/auth';
 
 
 export const logIn = async ( {email, password}, onSuccess, onError) => {
@@ -27,6 +27,16 @@ export const signUp = async ( {name, email, password}, onSuccess, onError) => {
       await updateProfile(auth.currentUser, {name});
       return onSuccess(user);
     }
+  } catch (error) {
+    return onError(error);
+  }
+}
+
+export const resetPassword = async ( {email}, onSuccess, onError) => {
+  try {
+    await sendPasswordResetEmail(auth, email);
+    Alert.alert("Password reset email sent");
+    return onSuccess;
   } catch (error) {
     return onError(error);
   }
