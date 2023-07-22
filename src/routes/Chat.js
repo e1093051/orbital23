17d29f174ff4798cc0160dd9c362c665257b36e9
e1093051 <e1093051@u.nus.edu>
2023-07-22 23:28:09 +0800

    chat

--- src/routes/Chat.js
+++ src/routes/Chat.js
@@ -60,9 +60,6 @@ export default function Chat() {
     }
   }, [isFocus])
 
-  const updateVariables = (render) => {
-    setFilterModule(render);
-  };
 
   async function loadChat() {
     if (profileData != null) {
@@ -71,15 +68,15 @@ export default function Chat() {
       const promises = chatList.map(async ([key, value]) => {
         const keySnapshot = await getDoc(doc(db, 'NUS/users', 'profile', key));
         const valueSnapshot = await getDoc(doc(db, 'NUS/chat', 'chat', value));
-      
+
         return {
-          frinedId: key, 
+          frinedId: key,
           id: value,
           key: keySnapshot.data(),
           value: valueSnapshot.data()
         };
       });
-      
+
 
       const mappedChatList = await Promise.all(promises);
 
@@ -91,7 +88,7 @@ export default function Chat() {
       });
 
       setChat(mappedChatList);
-      console.log(mappedChatList[2].id);
+
     }
   }
 
@@ -100,77 +97,82 @@ export default function Chat() {
 
 
   const renderChatItem = ({ item }) => {
-    const currentUserUid = auth.currentUser.uid;
-    const countMinusCurrentUserValue = item.value.count - item.value[currentUserUid];
+    if (!item || item.value.count === 0) {
+      return;
+    }
+    else {
+      const currentUserUid = auth.currentUser.uid;
+      const countMinusCurrentUserValue = item.value.count - item.value[currentUserUid];
 
-    const timestamp = item.value.timestamp.toDate();
+      const timestamp = item.value.timestamp.toDate();
 
-    const isSameDay = (date1, date2) =>
-      date1.getDate() === date2.getDate() &&
-      date1.getMonth() === date2.getMonth() &&
-      date1.getFullYear() === date2.getFullYear();
+      const isSameDay = (date1, date2) =>
+        date1.getDate() === date2.getDate() &&
+        date1.getMonth() === date2.getMonth() &&
+        date1.getFullYear() === date2.getFullYear();
 
-    const isSameYear = (date1, date2) => date1.getFullYear() === date2.getFullYear();
+      const isSameYear = (date1, date2) => date1.getFullYear() === date2.getFullYear();
 
 
 
-    // Function to format timestamp
-    const formatTimestamp = (date) => {
-      const today = new Date();
-      const yesterday = new Date(today);
-      yesterday.setDate(yesterday.getDate() - 1);
+      // Function to format timestamp
+      const formatTimestamp = (date) => {
+        const today = new Date();
+        const yesterday = new Date(today);
+        yesterday.setDate(yesterday.getDate() - 1);
 
-      if (isSameDay(date, today)) {
-        // If timestamp is today, show time only
-        return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-      } else if (isSameDay(date, yesterday)) {
-        // If timestamp is yesterday, show "Yesterday"
-        return 'Yesterday';
-      } else if (isSameYear(date, today)) {
-        // If timestamp is this year, show month and day
-        return date.toLocaleString('default', { month: 'short', day: 'numeric' });
-      } else {
-        // Otherwise, show year, month, and day
-        return date.toLocaleString('default', { year: 'numeric', month: 'short', day: 'numeric' });
-      }
-    };
+        if (isSameDay(date, today)) {
+          // If timestamp is today, show time only
+          return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+        } else if (isSameDay(date, yesterday)) {
+          // If timestamp is yesterday, show "Yesterday"
+          return 'Yesterday';
+        } else if (isSameYear(date, today)) {
+          // If timestamp is this year, show month and day
+          return date.toLocaleString('default', { month: 'short', day: 'numeric' });
+        } else {
+          // Otherwise, show year, month, and day
+          return date.toLocaleString('default', { year: 'numeric', month: 'short', day: 'numeric' });
+        }
+      };
 
-    const showTimeStamp = formatTimestamp(timestamp);
+      const showTimeStamp = formatTimestamp(timestamp);
 
-    return (
-      <View>
-        <TouchableOpacity onPress={() => {navigation.navigate("ChatPage", { updateVariables, name: item.key.name, id: item.id, firstMessage: item.value.firstMessage, friend: item.frinedId, render: render})}}>
-      <View style={{ borderBottomWidth: 1, width: Dimensions.get('window').width - 4, marginLeft: 1, paddingLeft: 8, paddingRight: 8, flexDirection: 'row', paddingVertical: 6, borderBottomColor: '#e5e5e5', justifyContent: 'space-between', paddingRight: 20 }}>
-        <View style={{ flexDirection: 'row' }}>
-          <Image
-            style={{
-              width: 50,
-              height: 50,
-              borderRadius: 200,
-            }}
-            source={{ uri: item.key.photoURL }}
-          />
-          <View style={{ paddingLeft: 20, paddingTop: 3, flexDirection: 'column' }}>
-            <View>
-              <Text style={{ fontWeight: '600', fontSize: 14 }}>{item.key.name}</Text>
-            </View>
-            <View style={{ paddingTop: 3 }}>
-              <Text>{item.value.lastMessage}</Text>
-            </View>
-          </View>
-        </View>
-        <View style = {{alignItems: 'flex-end', justifyContent: 'flex-start'}}>
+      return (
         <View>
-            <Text style = {{fontSize: 13}}>{showTimeStamp}</Text>
-          </View>
-          {countMinusCurrentUserValue != 0 && <View style={{ justifyContent: 'center', alignItems: 'center', backgroundColor: '#2de0ff', borderRadius: 20, width: 23, height: 23, marginTop: 5}}>
-            <Text style = {{color: 'white', fontWeight: '600'}}>{countMinusCurrentUserValue}</Text>
-          </View>}
+          <TouchableOpacity onPress={() => { navigation.navigate("ChatPage", { name: item.key.name, id: item.id, friend: item.frinedId, lastMessageTimestamp: item.value.timestamp }) }}>
+            <View style={{ borderBottomWidth: 1, width: Dimensions.get('window').width - 4, marginLeft: 1, paddingLeft: 8, paddingRight: 8, flexDirection: 'row', paddingVertical: 6, borderBottomColor: '#e5e5e5', justifyContent: 'space-between', paddingRight: 20 }}>
+              <View style={{ flexDirection: 'row' }}>
+                <Image
+                  style={{
+                    width: 50,
+                    height: 50,
+                    borderRadius: 200,
+                  }}
+                  source={{ uri: item.key.photoURL }}
+                />
+                <View style={{ paddingLeft: 20, paddingTop: 3, flexDirection: 'column' }}>
+                  <View>
+                    <Text style={{ fontWeight: '600', fontSize: 14 }}>{item.key.name}</Text>
+                  </View>
+                  <View style={{ paddingTop: 3 }}>
+                    <Text>{item.value.lastMessage}</Text>
+                  </View>
+                </View>
+              </View>
+              <View style={{ alignItems: 'flex-end', justifyContent: 'flex-start' }}>
+                <View>
+                  <Text style={{ fontSize: 13 }}>{showTimeStamp}</Text>
+                </View>
+                {countMinusCurrentUserValue != 0 && <View style={{ justifyContent: 'center', alignItems: 'center', backgroundColor: '#2de0ff', borderRadius: 20, width: 23, height: 23, marginTop: 5 }}>
+                  <Text style={{ color: 'white', fontWeight: '600' }}>{countMinusCurrentUserValue}</Text>
+                </View>}
+              </View>
+            </View>
+          </TouchableOpacity>
         </View>
-      </View>
-      </TouchableOpacity>
-      </View>
-    );
+      );
+    }
   };
 
 
--- src/routes/FriendProfile.js
+++ src/routes/FriendProfile.js
@@ -12,7 +12,7 @@ import {
 } from 'react-native';
 
 import { db, auth } from '../../api/fireConfig';
-import { useRoute } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 
 import { addDoc, collection, setDoc, doc, updateDoc, getDoc, onSnapshot } from "firebase/firestore";
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
@@ -20,17 +20,87 @@ import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 
 export default () => {
   const route = useRoute();
+  const navigation = useNavigation();
   const { uid } = route.params;
   const [profileData1, setProfileData1] = useState(null);
-  const getData = async () => {
+  const [profileData, setProfileData] = useState(null);
+
+  const getData1 = async () => {
     onSnapshot(doc(db, "NUS/users", "profile", uid), (doc) => {
       setProfileData1(doc.data());
     })
   }
 
+  const getData = async () => {
+    onSnapshot(doc(db, "NUS/users", "profile", auth.currentUser.uid), (doc) => {
+      setProfileData(doc.data());
+    })
+  }
+
+  const createNewChatDocument = async () => {
+    try {
+      const chatCollectionRef = collection(db, 'NUS', 'chat', 'chat');
+      const newChatDocRef = await addDoc(chatCollectionRef, {
+        count: 0,
+        [auth.currentUser.uid]: 0,
+        [uid]: 0
+      });
+
+      const newChatDocId = newChatDocRef.id;
+
+      return newChatDocId;
+    } catch (error) {
+      console.error('Error creating new chat document:', error);
+      return null; // Return null in case of an error
+    }
+  };
+
+  const checkIfKeyExistsWithValue = (mapping, key) => {
+    // Check if the key exists in the mapping object and its value matches the provided value
+    return mapping.hasOwnProperty(key);
+  };
+
+  const handleChat = async () => {
+    if (profileData) {
+      //const search = checkIfKeyExistsWithValue(profileData.chat, uid);
+      if (profileData.chat && checkIfKeyExistsWithValue(profileData.chat, uid)) {
+        //console.log("Hi")
+        const chatId = profileData.chat[uid];
+        const snapshot = await getDoc(doc(db, 'NUS/chat', 'chat', chatId));
+        const chatDoc = snapshot.data();
+        navigation.navigate("ChatPage", { name: profileData1.name, id: chatId, friend: uid })
+      }
+      else {
+        const chatId = await createNewChatDocument();
+        await setDoc(doc(db, "NUS", "users", "profile", `${auth.currentUser.uid}`), {
+          "chat": {
+            [uid]: chatId
+          }
+        },
+          { merge: true });
+        await setDoc(doc(db, "NUS", "users", "profile", uid), {
+          "chat": {
+            [auth.currentUser.uid]: chatId
+          }
+        },
+          { merge: true });
+        console.log("Just built a new chat");
+        navigation.navigate("ChatPage", { name: profileData1.name, id: chatId, friend: uid }) 
+      }
+    }
+  }
+
+
+
   useEffect(() => {
     getData();
   }, [])
+
+
+  useEffect(() => {
+    getData1();
+  }, [])
+
 
   return (
     <ScrollView style={{ backgroundColor: 'white' }}>
@@ -54,18 +124,17 @@ export default () => {
               {profileData1 && (<Text style={{ fontSize: 14, color: 'white' }}>{profileData1.bio}</Text>)}
             </View>
           </View>
-
         </View>
       </View>
       <View style={{ alignItems: 'center', flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 10, marginTop: -6 }}>
         <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
-          <TouchableOpacity style={{ backgroundColor: '#e5e5e5', width: Dimensions.get('window').width - 60, alignItems: 'center', height: 30, justifyContent: 'center' }}>
+          <TouchableOpacity style={{ backgroundColor: '#e5e5e5', width: Dimensions.get('window').width - 60, alignItems: 'center', height: 30, justifyContent: 'center' }} onPress={() => handleChat()}>
             <Text>Chat</Text>
           </TouchableOpacity>
         </View>
         <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
           <TouchableOpacity style={{ backgroundColor: '#e5e5e5', width: 30, alignItems: 'center', height: 30, justifyContent: 'center' }}>
-          <FontAwesome5 color="black" name="user-friends" size={12} />
+            <FontAwesome5 color="black" name="user-friends" size={12} />
           </TouchableOpacity>
         </View>
       </View>
