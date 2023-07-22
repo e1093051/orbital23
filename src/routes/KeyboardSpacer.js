32d278eafe9ee02fd1f8ce8539fcf465f630b344
e1093051 <e1093051@u.nus.edu>
2023-07-22 22:58:45 +0800

    chat

--- src/routes/KeyboardSpacer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { View, Keyboard } from 'react-native';
-
-const KeyboardSpacer = () => {
-  return <View style={{ flex: 1 }} onLayout={Keyboard.dismiss} />;
-};
-
-export default KeyboardSpacer;
